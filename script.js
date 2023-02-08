03b73574de05230728964e5bb85fd18c68294e91
Sergey Ilyin <gray-sergey@mail.ru>
2023-02-08 18:17:49 +0400

    v1

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
 // Wait for the document to load before running the script 
 (function ($) {
   
-  // We use some Javascript and the URL #fragment to hide/show different parts of the page
-  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Linking_to_an_element_on_the_same_page
-  $(window).on('load hashchange', function(){
-    
-    // First hide all content regions, then show the content-region specified in the URL hash 
-    // (or if no hash URL is found, default to first menu item)
-    $('.content-region').hide();
-    
-    // Remove any active classes on the main-menu
-    $('.main-menu a').removeClass('active');
-    var region = location.hash.toString() || $('.main-menu a:first').attr('href');
-    
-    // Now show the region specified in the URL hash
-    $(region).show();
-    
-    // Highlight the menu link associated with this region by adding the .active CSS class
-    $('.main-menu a[href="'+ region +'"]').addClass('active'); 
+  let $inputs = [1,2,3,4,5].map(x => $(`word${x}`))
+  let $result = $('result')
 
-    // Alternate method: Use AJAX to load the contents of an external file into a div based on URL fragment
-    // This will extract the region name from URL hash, and then load [region].html into the main #content div
-    // var region = location.hash.toString() || '#first';
-    // $('#content').load(region.slice(1) + '.html')
-    
-  });
+  let updateResult = () => {
+    try {
+      let filterStrs = $inputs.map($input => $input.value()).filter(x => !!x)
   
+      let applyFilterStr = (filterStr, words) => {
+        // ба!нан? 
+        let rules = []
+
+        let index = 0
+        let filter = filterStr.toLowerCase()
+        while (filter){
+          let letter = filter[0]
+          let nextLetter = filter[1]
+          if(nextLetter === '!')          {
+            rules.push(word => word[index] === letter)
+          }
+          else if(nextLetter === '?')          {
+            rules.push(word => word[index] !== letter)
+            rules.push(word => word.includes(letter))
+          }
+          else {
+            rules.push(word => !word.includes(letter))
+          }
+
+          index++
+        }        
+
+        let isFitToFilter = word => rules.every(rule => rule(word))
+        
+        return words.filter(isFitToFilter)
+      }
+      let resultWords = filterStrs.reduce(applyFilterStr, words5Array)
+      let resultStr = resultWords.slice(0, 20).join('\n')
+
+      $result.value(resultStr)
+    }
+    catch (e) {
+      $result.value(`Error: ${e}`)
+    }
+  }
+
+  $inputs.map($input => $input.on('change', updateResult))
+
 })(jQuery);
